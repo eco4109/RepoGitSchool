519f85bde6b05caba51bab10e5ee5cae001b8de1
@eco4109 <eco-4109@hotmail.com>
2018-09-14 16:23:08 -0500

    Actualizacion Arbol genealogco

--- Sistemas Expertos/prueba1.pl
+++ Sistemas Expertos/prueba1.pl
@@ -1,8 +1,16 @@
 padre(javier,xavier).
-madre(martha,xavier).
-padre(andres,javier). padre(andres,andreschico). padre(andres,gabino). padre(andres,graciela). padre(andres,noe). padre(andres,raul). padre(andres,elvia).
-
-padre(benja,martha). padre(benja,alicia). padre(benja,ismael). padre(benja,rogelio). padre(benja,juan).
+padre(andres,javier). 
+padre(andres,andreschico). 
+padre(andres,gabino). 
+padre(andres,graciela). 
+padre(andres,noe). 
+padre(andres,raul). 
+padre(andres,elvia).	
+padre(benja,martha). 
+padre(benja,alicia). 
+padre(benja,ismael). 
+padre(benja,rogelio). 
+padre(benja,juan).
 
 hermana(cecilia,xavier).
 
@@ -13,33 +21,72 @@ abuelom(benja,xavier).
 abuelom(benja,cecilia).
 
 abuelap(juana,xavier).
+
 abuenap(juana,cecilia).
 
 abuelop(andres,xavier).
 abuelop(andres,cecilia).
 
-hijo(javier,andres). hijo(andreschico,andres). hijo(gabino,andres). hijo(graciela,andres). hijo(noe,andres). hijo(raul,andres). hijo(elvia,andres).
+hijo(javier,andres). 
+hijo(andreschico,andres). 
+hijo(gabino,andres). 
+hijo(graciela,andres). 
+hijo(noe,andres). 
+hijo(raul,andres). 
+hijo(elvia,andres).
+hijo(javier,juana).
+hijo(raul,juana). 
+hijo(elvia,juana).
+hijo(andreschico,juana). 
+hijo(gabino,juana). 
+hijo(graciela,juana). 
+hijo(noe,juana).
+hijo(martha,benja). 
+hijo(alicia,benja). 
+hijo(ismael,benja). 
+hijo(rogelio,benja). 
+hijo(juan,benja).
+hijo(martha,paula). 
+hijo(alicia,paula). 
+hijo(ismael,paula). 
+hijo(rogelio,paula). 
+hijo(juan,paula).
+hijo(andrea,andreschico). 
+hijo(juan_jose,andreschico).
+hijo(andrea,marce). 
+hijo(juan_jose,marce).
+hijo(july,rogelio). 
+hijo(roge,rogelio). 
+hijo(erik,rogelio).
+hijo(july,mary). 
+hijo(roge,mary). 
+hijo(erik,mary).
+hijo(xavier,javier). 
+hijo(cecilia,javier).
+hijo(xavier,martha). 
+hijo(cecilia,martha).
 
-hijo(javier,juana). hijo(raul,juana). hijo(elvia,juana).
-hijo(andreschico,juana). hijo(gabino,juana). hijo(graciela,juana). hijo(noe,juana).
+madre(juana,javier). 
+madre(juana,andreschico). 
+madre(juana,gabino). 
+madre(juana,graciela). 
+madre(juana,noe). 
+madre(juana,raul). 
+madre(juana,elvia).
+madre(paula,martha). 
+madre(paula,alicia). 
+madre(paula,ismael). 
+madre(paula,rogelio). 
+madre(paula,juan).
+madre(martha,xavier).
 
-hijo(martha,benja). hijo(alicia,benja). hijo(ismael,benja). hijo(rogelio,benja). hijo(juan,benja).
+esposo(javier,martha). 
+esposo(andreschico,marce). 
+esposo(rogelio,mary). 
 
-hijo(martha,paula). hijo(alicia,paula). hijo(ismael,paula). hijo(rogelio,paula). hijo(juan,paula).
+esposa(mary,rogelio).
+esposa(marce,andreschico).
+esposa(martha,javier).
 
-
-madre(juana,javier). madre(juana,andreschico). madre(juana,gabino). madre(juana,graciela). madre(juana,noe). madre(juana,raul). madre(juana,elvia).
-madre(paula,martha). madre(paula,alicia). madre(paula,ismael). madre(paula,rogelio). madre(paula,juan).
-
-
-
-hijo(andrea,andreschico). hijo(juan_jose,andreschico).
-hijo(andrea,marce). hijo(juan_jose,marce).
-hijo(july,rogelio). hijo(roge,rogelio). hijo(erik,rogelio).
-hijo(july,mary). hijo(roge,mary). hijo(erik,mary).
-esposo(rogelio,mary). esposa(mary,rogelio).
-esposo(andreschico,marce). esposa(marce,andreschico).
-hijo(xavier,javier). hijo(cecilia,javier).
-hijo(xavier,martha). hijo(cecilia,martha).
-esposo(javier,martha). esposa(martha,javier).
+%Árbol genealogico de  Diego
 
