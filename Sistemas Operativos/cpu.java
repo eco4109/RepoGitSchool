440d1315fbda316ab37bc48e6e5e8430e8256740
xmendoxav <37055047+xmendoxav@users.noreply.github.com>
2018-09-23 21:22:50 -0500

    jhkj
    ljh

---
+++ Sistemas Operativos/cpu.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class cpu {
+	
+		public static int[] R = new int[14];
+		public static int[] B = new int[16];
+		/*Esto se utiliza como vector,
+		 por que funciona como banderas, quiere decir que declaramos un vector de 16 banderas*/
+		public static boolean[] psw = new boolean[16];
+		public static byte [] ram = new byte[1048576];
+	
+	public static void capta(){
+
+	}
+	public static void traduce(){
+
+	}
+	public static void ejecuta(){
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+	
+}
