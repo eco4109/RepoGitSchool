ea43d84d2184b8f89d837665f27ceda0dd9070e1
eco4109 <37058383+eco4109@users.noreply.github.com>
2018-10-02 00:12:30 -0500

    Actualizacion
    Practica 3 Expertos

--- Sistemas Expertos/eha2.java
+++ Sistemas Expertos/eha2.java
@@ -34,9 +34,9 @@ public class eha2{
 						System.out.println("             -----------------   SUB-MEN\u00da 1   --------------- ");
 						System.out.println("\n             Aproximadamente cual es su peso maximo en etapa adulta?\n");
 						System.out.println("                                 1.- De 0 a 500g.");
-						System.out.println("                                 2.- De 500 a 2Kg.");
-						System.out.println("                                 3.- De 2Kg a 70 Kg.");
-						System.out.println("                                 4.- De 2Kg a 70 Kg.");
+						System.out.println("                                 2.- De 500 a 5Kg.");
+						System.out.println("                                 3.- De 5Kg a 70 Kg.");
+						System.out.println("                                 4.- Mas de 70Kg.");
 						System.out.println("\n");
 						System.out.println("                                 5.- Regresar Menu anterior.");
 						System.out.print("              Teclee una opcion: ");
@@ -89,217 +89,156 @@ public class eha2{
 										System.out.println("                         TU MASCOTA ES UN PERRO RAZA PEQUEÑA.");
 										System.exit(4);
 									}else if(opcionS2.equals("2")==true){
-										System.out.println("                         TU MASCOTA ES UN GATO O UN HURON.");
+										System.out.println("                         TU MASCOTA ES UN GATO O UN HURON O un CONEJO.");
 										System.exit(4);
 									}
 								}while((opcionS2.equals("3")) != true);
 							break;
 							
 							case "3":
-								do{
-									System.out.println("\t");
-									System.out.println("\t");
-									System.out.println("             -----------------   SUB-MEN\u00da 1.C  --------------- ");
-									System.out.println("\t");
-									System.out.println("                                 1.- Opc. 1");
-									System.out.println("                                 2.- Opc. 2");
-									System.out.println("                                 3.- Opc. 3");
-									System.out.println("                                 4.- Opc. 4");
-									System.out.println("                                 5.- Regresar Menu anterior.");
-									System.out.print("              Teclee una opcion: ");
-									opcionS2 = teclado.nextLine();
-									if ((opcionS2.equals("1")==false) && (opcionS2.equals("2")==false) && (opcionS2.equals("3")==false) && (opcionS2.equals("4")==false) &&(opcionS2.equals("5")==false)) {
-										System.out.println('\7');
-									}
-								}while((opcionS2.equals("5")) != true);
-							break;
+								System.out.println("TU MASCOTA ESS UN PERRO RAZA MEDIANA-GRANDE");
+								break;
 							
 							case "4":
 								do{
 									System.out.println("\t");
 									System.out.println("\t");
-									System.out.println("             -----------------   SUB-MEN\u00da 1.C   ----------------- ");
-									System.out.println("\t");
-									System.out.println("                                 1.- Opcion 1");
-									System.out.println("                                 2.- Opcion 2");
-									System.out.println("                                 3.- Opcion 3");
-									System.out.println("                                 4.- Opcion 4");
-									System.out.println("                                 5.- Regresar al Menu anterior.");
+									System.out.println("             -----------------   SUB-MEN\u00da 1.D   ----------------- ");
+									System.out.println("\n           La gente suele comerce a tu mascota?");
+									System.out.println("                                 1.- Si.");
+									System.out.println("                                 2.- No.\n");
+									System.out.println("                                 3.- Regresar al Menu anterior.");
 									System.out.print("              Teclee una opcion: ");
 									opcionS2 = teclado.nextLine();
-									if ((opcionS2.equals("1")==false) && (opcionS2.equals("2")==false) && (opcionS2.equals("3")==false) && (opcionS2.equals("4")==false) &&(opcionS2.equals("5")==false)) {
+									if ((opcionS2.equals("1")==false) && (opcionS2.equals("2")==false)) {
 										System.out.println('\7');
-									}
-								}while((opcionS2.equals("5")) != true);
-							break;
-							case "5":
-								do{
-									System.out.println("\t");
-									System.out.println("\t");
-									System.out.println("             -----------------   SUB-MEN\u00da 1.D   ---------------- ");
-									System.out.println("\t");
-									System.out.println("                                 1.- Opcion 1");
-									System.out.println("                                 2.- Opcion 2");
-									System.out.println("                                 3.- Opcion 3");
-									System.out.println("                                 4.- Opcion 4");
-									System.out.println("                                 5.- Regresar al Menu anterior.");
-									System.out.print("              Teclee una opcion: ");
-									opcionS2 = teclado.nextLine();
-									if ((opcionS2.equals("1")==false) && (opcionS2.equals("2")==false) && (opcionS2.equals("3")==false) && (opcionS2.equals("4")==false) &&(opcionS2.equals("5")==false)) {
-										System.out.println('\7');
-									}
-								}while((opcionS2.equals("5")) != true);
-							break;
-					}
+									}else if(opcionS2.equals("1")==true){
+										System.out.println("\tTu mascota es un CERDO, OVEJA o una VACA, en algunos lugares un caballo");
+										System.exit(4);
+									}else if(opcionS2.equals("2")==true){
+										System.out.println("\tTu mascota es un BURRO, un CAMELLO, un CABALLO o un PERRO muy grande.");
+										System.exit(4);									}
+								}while((opcionS2.equals("3")) != true);
+								break;
+							
+						}
 					}while((opcionS.equals("5")) != true);
 					break;
 					
 				case "2":
-				do{
-					System.out.println("\t");
-					System.out.println("\t");
-					System.out.println("             =================   SUB-MEN\u00da 2   ============== ");
-					System.out.println("\t");
-					System.out.println("                                 1.- Opcion A");
-					System.out.println("                                 2.- Opcion B");
-					System.out.println("                                 3.- Opcion C");
-					System.out.println("                                 4.- Opcion D");
-					System.out.println("                                 5.- Regresar al Menu principal.");
-					System.out.print("              Teclee una opcion: ");
-					opcionS = teclado.nextLine();
-					if ((opcionS.equals("1")==false) && (opcionS.equals("2")==false) && (opcionS.equals("3")==false) && (opcionS.equals("4")==false) &&(opcionS.equals("5")==false)) {
-						System.out.println('\7');
-					}
-					switch(opcionS){
-						case "1":
-						do{
-							System.out.println("\t");
-							System.out.println("\t");
-							System.out.println("             -----------------   SUB-MEN\u00da  2.A  ------------------ ");
-							System.out.println("\t");
-							System.out.println("                                 1.- Opc. 1");
-							System.out.println("                                 2.- Opc. 2");
-							System.out.println("                                 3.- Opc. 3");
-							System.out.println("                                 4.- Opc. 4");
-							System.out.println("                                 5.- Regresar Menu anterior.");
-							System.out.print("              Teclee una opcion: ");
-							opcionS2 = teclado.nextLine();
-							if ((opcionS2.equals("1")==false) && (opcionS2.equals("2")==false) && (opcionS2.equals("3")==false) && (opcionS2.equals("4")==false) &&(opcionS2.equals("5")==false)) {
-								System.out.println('\7');
-							}
-						}while((opcionS2.equals("5")) != true);
-						break;
-						case "2":
-						do{
-							System.out.println("\t");
-							System.out.println("\t");
-							System.out.println("             ------------------   SUB-MEN\u00da 2.B   --------------- ");
-							System.out.println("\t");
-							System.out.println("                                 1.- Opc. 1");
-							System.out.println("                                 2.- Opc. 2");
-							System.out.println("                                 3.- Opc. 3");
-							System.out.println("                                 4.- Opc. 4");
-							System.out.println("                                 5.- Regresar  Menu anterior.");
-							System.out.print("              Teclee una opcion: ");
-							opcionS2 = teclado.nextLine();
-							if ((opcionS2.equals("1")==false) && (opcionS2.equals("2")==false) && (opcionS2.equals("3")==false) && (opcionS2.equals("4")==false) &&(opcionS2.equals("5")==false)) {
-								System.out.println('\7');
-							}
-						}while((opcionS2.equals("5")) != true);
-						break;
-						case "3":
-						do{
-							System.out.println("\t");
-							System.out.println("\t");
-							System.out.println("             ------------------   SUB-MEN\u00da 2.C   ----------------- ");
-							System.out.println("\t");
-							System.out.println("                                 1.- Opcion 1");
-							System.out.println("                                 2.- Opcion 2");
-							System.out.println("                                 3.- Opcion 3");
-							System.out.println("                                 4.- Opcion 4");
-							System.out.println("                                 5.- Regresar al Menu anterior.");
-							System.out.print("              Teclee una opcion: ");
-							opcionS2 = teclado.nextLine();
-							if ((opcionS2.equals("1")==false) && (opcionS2.equals("2")==false) && (opcionS2.equals("3")==false) && (opcionS2.equals("4")==false) &&(opcionS2.equals("5")==false)) {
-								System.out.println('\7');
-							}
-						}while((opcionS2.equals("5")) != true);
-						break;
-						case "4":
-						do{
-							System.out.println("\t");
-							System.out.println("\t");
-							System.out.println("             ------------------   SUB-MEN\u00da 2.D   ---------------- ");
-							System.out.println("\t");
-							System.out.println("                                 1.- Opc. 1");
-							System.out.println("                                 2.- Opc. 2");
-							System.out.println("                                 3.- Opc. 3");
-							System.out.println("                                 4.- Opc. 4");
-							System.out.println("                                 5.- Regresar  Menu anterior.");
-							System.out.print("              Teclee una opcion: ");
-							opcionS2 = teclado.nextLine();
-							if ((opcionS2.equals("1")==false) && (opcionS2.equals("2")==false) && (opcionS2.equals("3")==false) && (opcionS2.equals("4")==false) &&(opcionS2.equals("5")==false)) {
-								System.out.println('\7');
-							}
-						}while((opcionS2.equals("5")) != true);
-						break;
-				}
-					//Aqui va el SWITCH!!!!
-				}while((opcionS.equals("5")) != true);
-				break;
+					do{
+						System.out.println("\t");
+						System.out.println("\t");
+						System.out.println("             =================   SUB-MEN\u00da 2   ============== ");
+						System.out.println("\t  Tiene Patas?");
+						System.out.println("                                 1.- Si.");
+						System.out.println("                                 2.- No.");
+						System.out.println("\n");
+						System.out.println("                                 3.- Regresar al Menu principal.");
+						System.out.print("              Teclee una opcion: ");
+						opcionS = teclado.nextLine();
+						if ((opcionS.equals("1")==false) && (opcionS.equals("2")==false)&& (opcionS.equals("3")==false)) {
+							System.out.println('\7');
+						}
+						switch(opcionS){
+							case "1":
+								do{
+									System.out.println("\t");
+									System.out.println("\t");
+									System.out.println("             -----------------   SUB-MEN\u00da  2.A  ------------------ ");
+									System.out.println("\tSe mueve leeeeeeeeeento?");
+									System.out.println("                                 1.- Si.");
+									System.out.println("                                 2.- No.\n");
+									System.out.println("                                 3.- Regresar Menu anterior.");
+									System.out.print("              Teclee una opcion: ");
+									opcionS2 = teclado.nextLine();
+									if ((opcionS2.equals("1")==false) && (opcionS2.equals("2")==false)) {
+										System.out.println('\7');
+									}else if(opcionS2.equals("1")==true){
+										System.out.println("\tTu mascota es UNA TORTUGA");
+									}else if(opcionS2.equals("2")==true){
+										System.out.println("Tu mascota es UNA IGUANA, LAGARTIJA o un COCODRILO");
+									}
+								}while((opcionS2.equals("3")) != true);
+								break;
+							case "2":
+								do{
+									System.out.println("\t");
+									System.out.println("\t");
+									System.out.println("             ------------------   SUB-MEN\u00da 2.B   --------------- ");
+									System.out.println("\t  Si se mantiene fuera del agua muere?");
+									System.out.println("                                 1.- Si.");
+									System.out.println("                                 2.- No.\n");
+									System.out.println("                                 3.- Regresar  Menu anterior.");
+									System.out.print("              Teclee una opcion: ");
+									opcionS2 = teclado.nextLine();
+									if ((opcionS2.equals("1")==false) && (opcionS2.equals("2")==false)) {
+										System.out.println('\7');
+									}else if(opcionS2.equals("1")==true){
+										System.out.println("Tu mascota es UN PEZ");
+										System.exit(4);
+									}else if(opcionS2.equals("2")==true){
+										System.out.println("Tu mascota es UNA SERPIENTE");
+										System.exit(4);
+									}
+								}while((opcionS2.equals("3")) != true);
+								break;
+						}
+					}while((opcionS.equals("3")) != true);
+					break;
 				case "3":
-				do{
-					System.out.println("\t");
-					System.out.println("\t");
-					System.out.println("             ------------------   SUB-MEN\u00da 3   ---------------- ");
-					System.out.println("\t");
-					System.out.println("                                 1.- Opc. A");
-					System.out.println("                                 2.- Opc. B");
-					System.out.println("                                 3.- Opc. C");
-					System.out.println("                                 4.- Opc. D");
-					System.out.println("                                 5.- Regresar  Menu principal.");
-					System.out.print("              Teclee una opcion: ");
-					opcionS = teclado.nextLine();
-					if ((opcionS.equals("1")==false) && (opcionS.equals("2")==false) && (opcionS.equals("3")==false) && (opcionS.equals("4")==false) &&(opcionS.equals("5")==false)) {
-						System.out.println('\7');
-					}
-					switch(opcionS){
-						case "1":
-						do{
-							System.out.println("\t");
-							System.out.println("\t");
-							System.out.println("             -----------------   SUB-MEN\u00da  3.A  -------------- ");
-							System.out.println("\t");
-							System.out.println("                                 1.- Opcion 1");
-							System.out.println("                                 2.- Opcion 2");
-							System.out.println("                                 3.- Opcion 3");
-							System.out.println("                                 4.- Opcion 4");
-							System.out.println("                                 5.- Regresar al Menu anterior.");
-							System.out.print("              Teclee una opcion: ");
-							opcionS2 = teclado.nextLine();
-							if ((opcionS2.equals("1")==false) && (opcionS2.equals("2")==false) && (opcionS2.equals("3")==false) && (opcionS2.equals("4")==false) &&(opcionS2.equals("5")==false)) {
-								System.out.println('\7');
-							}
-						}while((opcionS2.equals("5")) != true);
-						break;
-						case "2":
-						do{
-							System.out.println("\t");
-							System.out.println("\t");
-							System.out.println("             -----------------   SUB-MEN\u00da 3.B   -------------- ");
-							System.out.println("\t");
-							System.out.println("                                 1.- Opcion 1");
-							System.out.println("                                 2.- Opcion 2");
-							System.out.println("                                 3.- Opcion 3");
-							System.out.println("                                 4.- Opcion 4");
-							System.out.println("                                 5.- Regresar al Menu anterior.");
-							System.out.print("              Teclee una opcion: ");
-							opcionS2 = teclado.nextLine();
-							if ((opcionS2.equals("1")==false) && (opcionS2.equals("2")==false) && (opcionS2.equals("3")==false) && (opcionS2.equals("4")==false) &&(opcionS2.equals("5")==false)) {
-								System.out.println('\7');
-							}
-						}while((opcionS2.equals("5")) != true);
-						break;
+					do{
+						System.out.println("\t");
+						System.out.println("\t");
+						System.out.println("             ------------------   SUB-MEN\u00da 3   ---------------- ");
+						System.out.println("\t");
+						System.out.println("                                 1.- Opc. A");
+						System.out.println("                                 2.- Opc. B");
+						System.out.println("                                 3.- Opc. C");
+						System.out.println("                                 4.- Opc. D");
+						System.out.println("                                 5.- Regresar  Menu principal.");
+						System.out.print("              Teclee una opcion: ");
+						opcionS = teclado.nextLine();
+						if ((opcionS.equals("1")==false) && (opcionS.equals("2")==false) && (opcionS.equals("3")==false) && (opcionS.equals("4")==false) &&(opcionS.equals("5")==false)) {
+							System.out.println('\7');
+						}
+						switch(opcionS){
+							case "1":
+								do{
+									System.out.println("\t");
+									System.out.println("\t");
+									System.out.println("             -----------------   SUB-MEN\u00da  3.A  -------------- ");
+									System.out.println("\t");
+									System.out.println("                                 1.- Opcion 1");
+									System.out.println("                                 2.- Opcion 2");
+									System.out.println("                                 3.- Opcion 3");
+									System.out.println("                                 4.- Opcion 4");
+									System.out.println("                                 5.- Regresar al Menu anterior.");
+									System.out.print("              Teclee una opcion: ");
+									opcionS2 = teclado.nextLine();
+									if ((opcionS2.equals("1")==false) && (opcionS2.equals("2")==false) && (opcionS2.equals("3")==false) && (opcionS2.equals("4")==false) &&(opcionS2.equals("5")==false)) {
+										System.out.println('\7');
+									}
+								}while((opcionS2.equals("5")) != true);
+								break;
+							case "2":
+							do{
+								System.out.println("\t");
+								System.out.println("\t");
+								System.out.println("             -----------------   SUB-MEN\u00da 3.B   -------------- ");
+								System.out.println("\t");
+								System.out.println("                                 1.- Opcion 1");
+								System.out.println("                                 2.- Opcion 2");
+								System.out.println("                                 3.- Opcion 3");
+								System.out.println("                                 4.- Opcion 4");
+								System.out.println("                                 5.- Regresar al Menu anterior.");
+								System.out.print("              Teclee una opcion: ");
+								opcionS2 = teclado.nextLine();
+								if ((opcionS2.equals("1")==false) && (opcionS2.equals("2")==false) && (opcionS2.equals("3")==false) && (opcionS2.equals("4")==false) &&(opcionS2.equals("5")==false)) {
+									System.out.println('\7');
+								}
+							}while((opcionS2.equals("5")) != true);
+							break;
 						case "3":
 						do{
 							System.out.println("\t");
