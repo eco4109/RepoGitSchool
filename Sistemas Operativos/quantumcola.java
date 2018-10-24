2e6c64c97794cebdec323c88e8b371ac253e7acf
eco4109 <37058383+eco4109@users.noreply.github.com>
2018-10-24 00:21:00 -0500

    Sistemas Operativos
    Tarea Cola-Quantum

--- Sistemas Operativos/quantumcola.java
+++ Sistemas Operativos/quantumcola.java
@@ -2,7 +2,29 @@ import java.io.*;
 import java.nio.*;
 import java.util.*;
 
-public class cola{
+class quantum{
+	static final long quantum = 1000;
+	
+	public static void generaInterrup(){
+		long horaSistema, horaInicial, diferencia;
+		
+		horaInicial = System.currentTimeMillis();
+		while(true){
+			horaSistema = System.currentTimeMillis();
+			diferencia = horaSistema - horaInicial;	
+			//System.out.println(diferencia);
+			if((diferencia >= quantum - 10)&&(diferencia <= quantum +10)){
+				System.out.println("¡INTERRUPCIÓN  :D  !\7");
+				horaInicial = horaSistema;
+				cola.insertarCola(1);
+				//System.out.println("Press Any Key To Continue...");
+          		//new java.util.Scanner(System.in).nextLine();
+			}
+		}
+	}	
+}
+
+class cola{
 	static int max = 10; //Tamaño máximo de la cola
 	static int[] cola = new int[max]; //Definición de la cola circular de tamaño "n"
 	static int inicioC, finC; //Indicadores del inicio y final de la cola
@@ -16,7 +38,9 @@ public class cola{
 
 	public static void insertarCola(int dato){ //Inserta en la cola un entero
 		if((finC == max-1 && inicioC == 0) || (finC+1==inicioC)){
-			System.out.println("\n¡COLA LLENA, TERMINA PROGRAMA!\n");
+			System.out.println("\n¡COLA LLENA:\n");
+			mostrarCola();
+			System.out.println("\n¡TERMINA PROGRAMA!\n");
 			System.exit(0);
 		}
 		if(finC==max-1 && inicioC!=0){
@@ -74,26 +98,17 @@ public class cola{
 			new java.util.Scanner(System.in).nextLine();
 		}
 	}
+}
 
 
 
-	public static void main(String[] args) {
-		inicializarCola();
-		insertarCola(1);
-		insertarCola(2);
-		insertarCola(3);
-		insertarCola(4);
-		insertarCola(5);
-		insertarCola(6);
-		insertarCola(7);
-		insertarCola(8);
-		insertarCola(9);
-		insertarCola(10);
-		mostrarCola();
-		removerCola();
-		insertarCola(11);
-		mostrarCola();
-		System.out.println("Programa terminado, presionar una tecla ... ...");
-		new java.util.Scanner(System.in).nextLine();
+
+
+
+
+public class quantumcola{
+	public static void main(String[] args){
+		cola.inicializarCola();
+		quantum.generaInterrup();
 	}
 }
